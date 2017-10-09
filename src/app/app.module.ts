956eb351b98f168469faf67b3a9eed43cd01ad79
Ramesh Kumar <ramesh@ghriantech.com>
2017-10-09 09:10:51 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {PolymerModule} from "@codebakery/origami"
+import {IronElementsModule, PaperElementsModule} from "@codebakery/origami/collections"
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PolymerModule.forRoot(),
+    PaperElementsModule,
+    IronElementsModule
   ],
   providers: [],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {webcomponentsReady} from "@codebakery/origami"
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -8,4 +9,13 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+webcomponentsReady().then(() =>{
+  platformBrowserDynamic().bootstrapModule(
+    AppModule,
+    {
+      enableLegacyTemplate: false
+    }
+  );
+});
+
+
